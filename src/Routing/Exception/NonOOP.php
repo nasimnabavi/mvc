65ee166210bb0a5429c20c0b203651f7c6ad6c2a
nasimnabavi <nasim.nabavi@scrutinizer-ci.com>
2016-04-29 16:50:04 +0200

    Update NonOOP.php

--- src/Routing/Exception/NonOOP.php
+++ src/Routing/Exception/NonOOP.php
@@ -1,16 +1,13 @@
 <?php
-
-namespace Zortje\MVC\Routing\Exception;
-
-use Zortje\MVC\Common\Exception\Exception;
-
-/**
- * Class RouteAlreadyConnectedException
- *
- * @package Zortje\MVC\Routing\Exception
- */
-public function testNonOOP($message) {
-	parent::__construct($message);
-}
-
-
+    function swap($a,$b){
+        print nl2br("Before swapping\n");
+        print nl2br("a = $a and b = $b \n\n\n");
+        $a = $a + $b; // 30 = 10 + 20
+        $b = $a - $b;
+        $a = $a - $b;
+        print nl2br("After swapping\n");
+        print nl2br("a = $a and b = $b \n");
+    }
+    swap(10,20);
+   
+?>
