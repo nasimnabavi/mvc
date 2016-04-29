7667fac7725cdc185515183835d5047b8c541e6a
nasimnabavi <nasim.nabavi@scrutinizer-ci.com>
2016-04-29 15:06:24 +0200

    Create NonOOP.php

---
+++ src/Routing/Exception/NonOOP.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Zortje\MVC\Routing\Exception;
+
+use Zortje\MVC\Common\Exception\Exception;
+
+/**
+ * Class RouteAlreadyConnectedException
+ *
+ * @package Zortje\MVC\Routing\Exception
+ */
+public function testNonOOP($message) {
+	parent::__construct($message);
+}
+
+
